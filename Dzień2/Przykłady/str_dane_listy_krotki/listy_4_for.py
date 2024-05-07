e56a9e14b636ee07e59395451f6e56f0abde7aef
mariusz7b1 <35932429+mariusz7b1@users.noreply.github.com>
2024-05-07 07:52:07 +0200

    Dzien2-start

---
+++ Dzień2/Przykłady/str_dane_listy_krotki/listy_4_for.py
@@ -0,0 +1,13 @@
+"""
+Listy - for
+"""
+from os import system
+system('cls')
+
+moja_lista = ["biały", "różowy", "niebieski", "żółty", "zielony"]
+for kolor in moja_lista:
+    print(kolor)
+
+#  z wyk indeksu
+for i in range(0, len(moja_lista)):
+    print(moja_lista[i])
