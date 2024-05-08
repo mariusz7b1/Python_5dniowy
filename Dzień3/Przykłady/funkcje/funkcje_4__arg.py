de78f361e9c6c809d4ca1c6aa7d53cb59a06b89d
mariusz7b1 <35932429+mariusz7b1@users.noreply.github.com>
2024-05-08 11:39:19 +0200

    kolejna

---
+++ Dzień3/Przykłady/funkcje/funkcje_4__arg.py
@@ -0,0 +1,21 @@
+"""
+Funkcje - tablica argumentów
+"""
+from os import system
+system('cls')
+
+
+def pokaz_arumenty(*args):
+    print(type(args))
+    print(f"Przekazano {len(args)} argumentów")
+    for war in args:
+        print(war, end=",")
+    # koniec funkcji
+
+
+pass
+pokaz_arumenty("ala", 1, 2.7, "ma", 88, 2.5)
+print()
+lst1 = [1, 2, 3]
+pokaz_arumenty(lst1)
+pokaz_arumenty(*lst1)
